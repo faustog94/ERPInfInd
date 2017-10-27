0c23c8162e3d7e62d91f292e82f40563f944a33d
francoance <franco.ance@gmail.com>
2017-10-27 15:38:22 -0300

    Sql 27/10 - Art, prod y centros cargados

--- InfInd.sql
+++ InfInd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Oct 16, 2017 at 07:17 PM
+-- Generation Time: Oct 27, 2017 at 06:36 PM
 -- Server version: 5.7.19
 -- PHP Version: 7.0.21
 
@@ -31,11 +31,50 @@ SET time_zone = "+00:00";
 CREATE TABLE `Articulo` (
   `idArticulo` int(10) NOT NULL,
   `codArticulo` varchar(50) NOT NULL,
-  `descripcion` varchar(50) NOT NULL,
+  `descripcion` varchar(150) NOT NULL,
   `activo` tinyint(1) NOT NULL,
   `unidadMedida` varchar(10) NOT NULL,
   `idTipoArticulo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Articulo`
+--
+
+INSERT INTO `Articulo` (`idArticulo`, `codArticulo`, `descripcion`, `activo`, `unidadMedida`, `idTipoArticulo`) VALUES
+(3, '1.1.2.01.001.1', 'Uva tinta Malbec DonPedro Sector B', 1, 'qq', 1),
+(4, '1.1.2.01.001.01', 'Uva blanca Chardonnay DonPedro Sector A', 1, 'qq', 1),
+(5, '1.2.01.01', 'Polvo de barrica de roble Francés ', 1, 'g', 1),
+(6, '1.2.02.01', 'Estabilizador de Color Taninno Fermotan', 1, 'g', 1),
+(7, '1.2.03.01', 'Rehidratador de Levadura Fermoplus Energy', 1, 'g', 1),
+(8, '1.2.03.02', 'Rehidratador de Levadura  Fermoplus Blanc', 1, 'g', 1),
+(9, '1.2.04.01', 'Levadura Fermol Rouge-Bayanus', 1, 'g', 1),
+(10, '1.2.04.02', 'Levadura Fermol Rouge', 1, 'g', 1),
+(11, '1.2.04.03', 'Levadura Fermol Premier Cru', 1, 'g', 1),
+(12, '1.2.04.04', 'Levadura Fermol Chardonnay', 1, 'g', 1),
+(13, '1.2.04.05', 'Levadura Fermol Blanc', 1, 'g', 1),
+(14, '1.2.05.01', 'Nutriente Fermocel', 1, 'g', 1),
+(15, '1.2.05.02', 'Nutriente Enovit', 1, 'g', 1),
+(16, '1.2.05.03', 'Batonnage Plus', 1, 'g', 1),
+(17, '1.2.05.04', 'Nutriente Microcel', 1, 'g', 1),
+(18, '1.2.05.5', 'Nutriente Fermoplus Rouge', 1, 'g', 1),
+(19, '1.2.06.01', 'Enzimas Endozym Rouge', 1, 'g', 1),
+(20, '1.2.06.02', 'Enzimas Endozym Cultivar', 1, 'g', 1),
+(21, '2.1.01', 'Botella transparente 1L', 1, 'un', 2),
+(22, '2.1.02', 'Botella oscura 1L', 1, 'un', 2),
+(23, '2.1.03', 'Botella transparente 750mL', 1, 'un', 2),
+(24, '2.1.04', 'Botella oscura 750mL', 1, 'un', 2),
+(25, '2.2.1', 'Corcho natural', 1, 'un', 2),
+(26, '2.2.2', 'Corcho sintético ', 1, 'un', 2),
+(27, '2.3.001', 'Etiqueta frontal tinto Malbec', 1, 'un', 2),
+(28, '2.3.002', 'Etiqueta frontal tinto Malbec', 1, 'un', 2),
+(29, '2.3.003', 'Etiqueta frontal blanco Chardonnay', 1, 'un', 2),
+(30, '2.3.004', 'Etiqueta trasera blanco Chardonnay', 1, 'un', 2),
+(31, '2.4', 'Capsula', 1, 'un', 2),
+(32, 'Mosto Tinto Malbec', '3.1.1.01', 1, 'L', 3),
+(33, '3.1.2.01', 'Mosto Blanco Chardonnay', 1, 'L', 3),
+(34, '3.2.1.01', 'Viño a añejar Tinto Malbec', 1, 'L', 3),
+(35, '3.2.2.01', 'Vino a añejar Blanco Chardonnay', 1, 'L', 3);
 
 -- --------------------------------------------------------
 
@@ -66,6 +105,19 @@ CREATE TABLE `CentroTrabajo` (
   `costoHoraManoObra` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `CentroTrabajo`
+--
+
+INSERT INTO `CentroTrabajo` (`idCentro`, `codCentro`, `descripcion`, `activo`, `costoHoraMaquinaria`, `costoHoraManoObra`) VALUES
+(2, '7.1.1', 'Área de despalillado', 1, 0, 0),
+(3, '7.1.2', 'Área de molienda', 1, 0, 0),
+(4, '7.1.3', 'Área de fermentación', 1, 0, 0),
+(5, '7.1.4', 'Área de estrujado', 1, 0, 0),
+(6, '7.1.5', 'Área de crianza', 1, 0, 0),
+(7, '7.1.6', 'Área de embotellado', 1, 0, 0),
+(8, '7.1.7', 'Área de envejecimiento', 1, 0, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -92,8 +144,7 @@ CREATE TABLE `DetalleRuta` (
   `tiempoMaquina` double NOT NULL,
   `tiempoReposo` double NOT NULL,
   `idRuta` int(10) NOT NULL,
-  `idCentro` int(10) NOT NULL,
-  `idEstructura` int(10) NOT NULL
+  `idCentro` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -187,6 +238,15 @@ CREATE TABLE `Producto` (
   `idTipoProducto` int(10) NOT NULL,
   `idProductoBase` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Producto`
+--
+
+INSERT INTO `Producto` (`idProducto`, `codProducto`, `descripcion`, `activo`, `unidadMedida`, `idTipoProducto`, `idProductoBase`) VALUES
+(7, '4.1.1.01', 'Vino Tinto Malbec 1L', 1, 'un', 2, 8),
+(8, '4.2.1', 'Mosto Tinto Malbec', 1, 'L', 1, 0),
+(9, '4.2.2.01', 'Mosto Vino Blanco Chardonnay', 1, 'L', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -324,7 +384,8 @@ ALTER TABLE `Estructura`
 -- Indexes for table `Maquinaria`
 --
 ALTER TABLE `Maquinaria`
-  ADD PRIMARY KEY (`idMaquinaria`);
+  ADD PRIMARY KEY (`idMaquinaria`),
+  ADD UNIQUE KEY `codMaquinaria` (`codMaquinaria`);
 
 --
 -- Indexes for table `Materiales`
@@ -383,7 +444,7 @@ ALTER TABLE `TipoProducto`
 -- AUTO_INCREMENT for table `Articulo`
 --
 ALTER TABLE `Articulo`
-  MODIFY `idArticulo` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idArticulo` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `ArtProv`
@@ -395,7 +456,7 @@ ALTER TABLE `ArtProv`
 -- AUTO_INCREMENT for table `CentroTrabajo`
 --
 ALTER TABLE `CentroTrabajo`
-  MODIFY `idCentro` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCentro` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `DetalleOrdenProd`
@@ -407,7 +468,7 @@ ALTER TABLE `DetalleOrdenProd`
 -- AUTO_INCREMENT for table `DetalleRuta`
 --
 ALTER TABLE `DetalleRuta`
-  MODIFY `idDetalleRuta` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `idDetalleRuta` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `EstadoOrdenProduccion`
@@ -419,7 +480,7 @@ ALTER TABLE `EstadoOrdenProduccion`
 -- AUTO_INCREMENT for table `Estructura`
 --
 ALTER TABLE `Estructura`
-  MODIFY `idEstructura` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `idEstructura` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `Maquinaria`
@@ -431,7 +492,7 @@ ALTER TABLE `Maquinaria`
 -- AUTO_INCREMENT for table `Materiales`
 --
 ALTER TABLE `Materiales`
-  MODIFY `idMateriales` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `idMateriales` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `OrdenProduccion`
@@ -443,7 +504,7 @@ ALTER TABLE `OrdenProduccion`
 -- AUTO_INCREMENT for table `Producto`
 --
 ALTER TABLE `Producto`
-  MODIFY `idProducto` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idProducto` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `Proveedor`
@@ -455,7 +516,7 @@ ALTER TABLE `Proveedor`
 -- AUTO_INCREMENT for table `RutaFabricacion`
 --
 ALTER TABLE `RutaFabricacion`
-  MODIFY `idRuta` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `idRuta` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `TipoArticulo`
